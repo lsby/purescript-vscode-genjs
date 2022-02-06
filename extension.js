66ce4b3cb579f38a5b4d559e99b8c6081d1b74c7
hbybyyang <2451759073@qq.com>
2022-02-06 14:05:02 +0800

    可以用了

--- extension.js
+++ extension.js
@@ -1,10 +1,43 @@
-const vscode = require("vscode");
+var vscode = require("vscode");
+var fs = require("fs");
+var path = require("path");
 
 function activate(context) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("genjs.helloWorld", function () {
-      vscode.window.showInformationMessage("Hello World from !");
-    })
+    vscode.commands.registerCommand(
+      "purescript-vscode-genjs.genjs",
+      function () {
+        var filePath = vscode.window.activeTextEditor.document.uri.fsPath;
+        var dirPath = path.dirname(filePath);
+        var fileName = path.basename(filePath);
+        var jsFileName = fileName.replace(".purs", ".js");
+        var jsFilePath = path.resolve(dirPath, jsFileName);
+
+        var code = fs.readFileSync(filePath).toString();
+        var genCode = code
+          .split("\n")
+          .filter((a) => /^foreign import/.test(a))
+          .filter((a) => !/^foreign import data/.test(a))
+          .map((a) =>
+            a.match(/foreign import (.*?)::(.*)/).map((a) => a.trim())
+          )
+          .map((a) =>
+            [`// ${a[1]} :: ${a[2]}`, `exports.${a[1]} = 1;`].join("\n")
+          )
+          .join("\n\n");
+
+        var oldCode = [];
+        if (fs.existsSync(jsFilePath)) {
+          oldCode = [
+            fs.readFileSync(jsFilePath).toString(),
+            "// ------------------",
+          ];
+        }
+
+        fs.writeFileSync(jsFilePath, [...oldCode, genCode, ""].join("\n"));
+        vscode.window.showInformationMessage("文件已生成");
+      }
+    )
   );
 }
 
